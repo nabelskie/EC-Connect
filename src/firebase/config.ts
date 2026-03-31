1a573056dc326f30edbe3dee70b457c93260dde7
Nabil Ikhwan <ikhwannabil1@gmail.com>
2026-03-31 13:31:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7284655464-82114",
+  "appId": "1:216338507130:web:9ff77c86fe89f97bfc62c3",
+  "apiKey": "AIzaSyB8q6Tf9GH2UKekY_jgEf3s5EE5ydU4tSE",
+  "authDomain": "studio-7284655464-82114.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "216338507130"
+};
